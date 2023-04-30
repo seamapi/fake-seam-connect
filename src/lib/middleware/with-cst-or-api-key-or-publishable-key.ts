b346cddee1edc27c4840cc093e0eba05a936e944
Severin Ibarluzea <seveibar@gmail.com>
2023-04-30 23:21:26 +0200

    progress towards create middleware

--- src/lib/middleware/with-cst-or-api-key-or-publishable-key.ts
+++ src/lib/middleware/with-cst-or-api-key-or-publishable-key.ts
@@ -2,24 +2,24 @@ import { type Middleware } from 'nextlove'
 
 import { type Database } from 'lib/database/index.ts'
 
-export const withApiKey: Middleware<
+export const withCSTOrApiKeyOrPublishableKey: Middleware<
   {
-    auth: { auth_mode: 'api_key' }
+    auth:
+      | { auth_mode: 'api_key'; workspace_id: string }
+      | { auth_mode: 'client_session_token'; workspace_id: string }
+      | { auth_mode: 'publishable_key'; workspace_id: string }
   },
   {
     db: Database
   }
 > = (next) => async (req, res) => {
-  if (req.db == null) {
-    return res
-      .status(500)
-      .end(
-        'The withApiKey middleware requires req.db. Use it with the withDb middleware.'
-      )
-  }
-
-  const token = req.headers.authorization?.split('Bearer ')?.[1]
+  const token =
+    req.headers.authorization?.split('Bearer ')?.[1] ??
+    req.headers['client-session-token']
   if (token == null) return res.status(401).end('Unauthorized')
+
+  const is_cst = token.includes('seam_cst')
+  const is_api_key = !is_cst
 
   // TODO: Validate authorization.
   // If relevant, add the user or the decoded JWT to the request on req.auth.
@@ -28,8 +28,8 @@ export const withApiKey: Middleware<
   return next(req, res)
 }
 
-withApiKey.securitySchema = {
+withCSTOrApiKeyOrPublishableKey.securitySchema = {
   type: 'apiKey',
 }
 
-export default withApiKey
+export default withCSTOrApiKeyOrPublishableKey
