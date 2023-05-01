98a51380edd3479c0adce428033424a92dbd26b3
Severin Ibarluzea <seveibar@gmail.com>
2023-05-01 19:35:57 +0200

    wip

--- src/lib/middleware/with-route-spec.ts
+++ src/lib/middleware/with-route-spec.ts
@@ -2,14 +2,16 @@ import { createWithRouteSpec } from 'nextlove'
 
 import withApiKey from './with-api-key.ts'
 import withDb from './with-db.ts'
+import { withCSTOrApiKeyOrPublishableKey } from './with-cst-or-api-key-or-publishable-key.ts'
 
 export const withRouteSpec = createWithRouteSpec({
-  apiName: 'Fake Template',
+  apiName: 'Fake Seam Connect',
   productionServerUrl: 'https://example.com',
   shouldValidateGetRequestBody: false,
   globalMiddlewares: [withDb],
   authMiddlewareMap: {
     api_key: withApiKey,
+    cst_ak_pk: withCSTOrApiKeyOrPublishableKey,
   },
 } as const)
 
