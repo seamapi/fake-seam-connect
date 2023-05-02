79085dabb52f10c900a9a57c5eae69c27ba28a32
Severin Ibarluzea <seveibar@gmail.com>
2023-05-03 01:14:15 +0200

    wip

---
+++ test/api/access_codes/create.test.ts
@@ -0,0 +1,24 @@
+import test, { type ExecutionContext } from "ava"
+
+import { getTestServer } from "fixtures/get-test-server.ts"
+
+test("POST /access_codes/create", async (t: ExecutionContext) => {
+  const { axios, seed } = await getTestServer(t)
+  const {
+    data: { access_code },
+  } = await axios.post(
+    "/access_codes/create",
+    {
+      device_id: seed.ws2.device1_id,
+      name: "Test Access Code",
+      code: "1234",
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${seed.ws2.cst}`,
+      },
+    }
+  )
+
+  t.is(access_code.code, "1234")
+})
--- test/fixtures/get-test-database.ts
+++ test/fixtures/get-test-database.ts
@@ -1,8 +1,8 @@
-import { type ExecutionContext } from 'ava'
+import { type ExecutionContext } from "ava"
 
-import { createDatabase, type Thing } from 'index.ts'
+import { createDatabase, type Thing } from "index.ts"
 
-import { type Database } from 'lib/database/types.ts'
+import { type Database } from "lib/database/types.ts"
 
 export interface DatabaseFixture {
   db: Database
@@ -17,9 +17,10 @@ interface Seed {
   ws2: {
     workspace_id: string
     publishable_key: string
-    // TODO
-    // connected_account1_id: string
-    // device1_id: string
+    cst: string
+    connected_account1_id: string
+    device1_id: string
+    device2_id: string
   }
 }
 
@@ -28,8 +29,44 @@ export const getTestDatabase = async (
 ): Promise<DatabaseFixture> => {
   const db = createDatabase()
 
-  const ws1 = db.addWorkspace({ name: 'Seed Workspace 1 (starts empty)' })
-  const ws2 = db.addWorkspace({ name: 'Seed Workspace 2 (starts populated)' })
+  const ws1 = db.addWorkspace({ name: "Seed Workspace 1 (starts empty)" })
+  const ws2 = db.addWorkspace({ name: "Seed Workspace 2 (starts populated)" })
+
+  const cw = db.addConnectWebview({
+    workspace_id: ws2.workspace_id,
+  })
+
+  const ca = db.addConnectedAccount({
+    provider: "august",
+    workspace_id: ws2.workspace_id,
+  })
+
+  db.updateConnectWebview({
+    connect_webview_id: cw.connect_webview_id,
+    connected_account_id: ca.connected_account_id,
+    status: "authorized",
+  })
+
+  const device1 = db.addDevice({
+    connected_account_id: ca.connected_account_id,
+    device_type: "august_lock",
+    name: "Front Door",
+    workspace_id: ws2.workspace_id,
+  })
+
+  const device2 = db.addDevice({
+    connected_account_id: ca.connected_account_id,
+    device_type: "august_lock",
+    name: "Back Door",
+    workspace_id: ws2.workspace_id,
+  })
+
+  const client_session = db.addClientSession({
+    workspace_id: ws2.workspace_id,
+    connect_webview_ids: [cw.connect_webview_id],
+    connected_account_ids: [ca.connected_account_id],
+    user_identifier_key: "seed_client_session_user",
+  })
 
   const seed = {
     ws1: {
@@ -39,6 +76,10 @@ export const getTestDatabase = async (
     ws2: {
       workspace_id: ws2.workspace_id,
       publishable_key: ws2.publishable_key,
+      cst: client_session.token,
+      connected_account1_id: ca.connected_account_id,
+      device1_id: device1.device_id,
+      device2_id: device2.device_id,
     },
   } as Seed
 
