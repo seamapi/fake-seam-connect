1e29b81e7b0ae47366623b582c607991f211c5aa
Severin Ibarluzea <seveibar@gmail.com>
2023-05-02 21:53:07 +0200

    fix client session create test

--- test/stories/cst-login-view-devices.test.ts
+++ test/stories/cst-login-view-devices.test.ts
@@ -1,20 +1,22 @@
-import test from 'ava'
-import { getTestServer } from 'fixtures/get-test-server.ts'
+import test from "ava"
+import { getTestServer } from "fixtures/get-test-server.ts"
 
-test('Login via a CST and view devices', async (t) => {
+test("Login via a CST and view devices", async (t) => {
   const { axios, seed } = await getTestServer(t)
-  const { data } = await axios.post(
-    '/internal/client_sessions/create',
+  const {
+    data: { client_session },
+  } = await axios.post(
+    "/internal/client_sessions/create",
     {
-      user_identifier_key: 'my_test_user',
+      user_identifier_key: "my_test_user",
     },
     {
       headers: {
-        'Seam-Publishable-Key': seed.ws1.publishable_key,
+        "Seam-Publishable-Key": seed.ws1.publishable_key,
       },
     }
   )
 
-  console.log(data)
-  // t.is(data.length, 2)
+  t.is(client_session.user_identifier_key, "my_test_user")
+  t.truthy(client_session.token)
 })
