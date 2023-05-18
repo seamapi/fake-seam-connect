4216df8a7907d1d25897131aef28ce1a699687f9
andrii-balitskyi <10balian10@gmail.com>
2023-05-18 13:40:53 +0200

    Add withCors middleware

---
+++ src/lib/middleware/with-cors.ts
@@ -0,0 +1,23 @@
+import { NextApiRequest, NextApiResponse } from "next"
+import { Middleware } from "nextlove"
+
+const withCors: Middleware<{}> =
+  (next) => (req: NextApiRequest, res: NextApiResponse) => {
+    res.setHeader("Access-Control-Allow-Origin", req.headers.origin ?? "")
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PUT, PATCH, DELETE, OPTIONS"
+    )
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization")
+    res.setHeader("Access-Control-Allow-Credentials", "true")
+
+    if (req.method === "OPTIONS") {
+      res.status(200).end()
+
+      return
+    }
+
+    return next(req, res)
+  }
+
+export default withCors
--- src/lib/middleware/with-route-spec.ts
+++ src/lib/middleware/with-route-spec.ts
@@ -3,12 +3,13 @@ import { createWithRouteSpec } from "nextlove"
 import withApiKey from "./with-api-key.ts"
 import { withCSTOrApiKeyOrPublishableKey } from "./with-cst-or-api-key-or-publishable-key.ts"
 import withDb from "./with-db.ts"
+import withCors from "./with-cors.ts"
 
 export const withRouteSpec = createWithRouteSpec({
   apiName: "Fake Seam Connect",
   productionServerUrl: "https://example.com",
   shouldValidateGetRequestBody: false,
-  globalMiddlewares: [withDb],
+  globalMiddlewares: [withCors, withDb],
   authMiddlewareMap: {
     api_key: withApiKey,
     cst_ak_pk: withCSTOrApiKeyOrPublishableKey,
