6d0a910e9738f2b8a3085d3c2b37e75301706229
Mike Wu <hello@mikewu.dev>
2023-05-18 11:39:37 +0900

    Add more device properties

--- src/lib/zod/device.ts
+++ src/lib/zod/device.ts
@@ -10,16 +10,31 @@ export const deviceType = z.enum([
 export const common_device_properties = z.object({
   online: z.boolean(),
   name: z.string(),
+  battery: z
+    .object({
+      level: z.number(),
+      status: z.enum(["critical", "low", "good", "full"]),
+    })
+    .optional(),
+  image_url: z.string().optional(),
 })
 
 export const lock_device_properties = common_device_properties.extend({
   locked: z.boolean(),
   door_open: z.boolean().optional(),
   battery_level: z.number().optional(),
+  has_direct_power: z.boolean().optional(),
+  manufacturer: z.string().optional(),
+  supported_code_lengths: z.array(z.number()).optional(),
+  max_active_codes_supported: z.number().optional(),
+  serial_number: z.string().optional(),
   schlage_metadata: z
     .object({
       device_id: z.string(),
       device_name: z.string(),
+      access_code_length: z.number(),
+      model: z.string().optional(),
+      location_id: z.string().optional(),
     })
     .optional(),
   august_metadata: z
@@ -27,8 +42,17 @@ export const lock_device_properties = common_device_properties.extend({
       lock_id: z.string(),
       lock_name: z.string(),
       house_name: z.string(),
+      has_keypad: z.boolean().optional(),
+      model: z.string().optional(),
+      keypad_battery_level: z.string().optional(),
     })
     .optional(),
+  nuki_metadata: z
+    .object({
+      keypad_battery_critical: z.boolean().optional(),
+    })
+    .optional(),
+  smartthings_metadata: z.any().optional(),
 })
 
 export const device = z.object({
