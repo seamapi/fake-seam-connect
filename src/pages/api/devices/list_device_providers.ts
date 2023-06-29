0311c07fff947a4ea7e2eef5b0a43dc3408ed86f
Mike Wu <hello@mikewu.dev>
2023-06-29 12:12:03 +0900

    feat: Add devices/list_device_providers endpoint

---
+++ src/pages/api/devices/list_device_providers.ts
@@ -0,0 +1,46 @@
+import { z } from "zod"
+
+import { device } from "lib/zod/index.ts"
+
+import { withRouteSpec } from "lib/middleware/with-route-spec.ts"
+
+const device_provider = z.object({
+  device_provider_name: z.string(),
+  display_name: z.string(),
+  image_url: z.string(),
+  provider_categories: z.array(
+    z.union([z.literal("stable"), z.literal("internal_beta")])
+  ),
+})
+
+const device_providers: Array<z.infer<typeof device_provider>> = [
+  {
+    device_provider_name: "august",
+    display_name: "August",
+    image_url: `https://connect.getseam.com/assets/images/logos/august_logo_square.png`,
+    provider_categories: [],
+  },
+  {
+    device_provider_name: "yale",
+    display_name: "Yale",
+    image_url: `https://connect.getseam.com/assets/images/logos/yale_logo_square.png`,
+    provider_categories: [],
+  },
+  {
+    device_provider_name: "lockly",
+    display_name: "Lockly",
+    image_url: `https://connect.getseam.com/assets/images/logos/lockly_logo_square.png`,
+    provider_categories: [],
+  },
+]
+
+export default withRouteSpec({
+  auth: "cst_ak_pk",
+  methods: ["GET", "POST"],
+  commonParams: z.object({}),
+  jsonResponse: z.object({
+    device_providers: z.array(device_provider),
+  }),
+} as const)(async (req, res) => {
+  res.status(200).json({ device_providers })
+})
