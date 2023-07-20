1adbdedbd9d26eb7eb6c192c3a81cf94ba7ad192
Evan Sosenko <evan@getseam.com>
2023-07-19 20:19:07 -0700

    Add back index page

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import type { ReactNode } from "react"
+
+export default (): ReactNode => (
+  <html>
+    <body>
+      <p>
+        Are you trying to access the Seam API?{" "}
+        <a href="https://docs.getseam.com">Check out the docs!</a>
+      </p>
+    </body>
+  </html>
+)
