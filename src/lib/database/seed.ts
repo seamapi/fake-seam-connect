6168a94d2d0d604ba292019df160163549a90c3b
Severin Ibarluzea <seveibar@gmail.com>
2023-07-20 17:43:55 -0700

    seed prefix

--- src/lib/database/seed.ts
+++ src/lib/database/seed.ts
@@ -3,8 +3,8 @@ import type { Database } from "./schema.ts"
 export type Seed = {
   john_connected_account_id: "john_connected_account_id"
   jane_connected_account_id: "jane_connected_account_id"
-  some_workspace_1: "some_workspace_1"
-  some_workspace_2: "some_workspace_2"
+  seed_workspace_1: "seed_workspace_1"
+  seed_workspace_2: "seed_workspace_2"
 }
 
 export const seed = (db: Database): Seed => {
@@ -18,19 +18,19 @@ export const seed = (db: Database): Seed => {
     )
   }
 
-  db.addWorkspace({ name: "My Workspace", workspace_id: "some_workspace_1" })
+  db.addWorkspace({ name: "My Workspace", workspace_id: "seed_workspace_1" })
   db.addWorkspace({
     name: "Empty Workspace",
-    workspace_id: "some_workspace_2",
+    workspace_id: "seed_workspace_2",
   })
 
   const cw = db.addConnectWebview({
-    workspace_id: "some_workspace_1",
+    workspace_id: "seed_workspace_1",
   })
 
   db.addConnectedAccount({
     provider: "august",
-    workspace_id: "some_workspace_1",
+    workspace_id: "seed_workspace_1",
     user_identifier: {
       email: "john@example.com",
     },
@@ -48,7 +48,7 @@ export const seed = (db: Database): Seed => {
     connected_account_id: "john_connected_account_id",
     device_type: "august_lock",
     name: "Front Door",
-    workspace_id: "some_workspace_1",
+    workspace_id: "seed_workspace_1",
   })
 
   db.addDevice({
@@ -56,12 +56,12 @@ export const seed = (db: Database): Seed => {
     connected_account_id: "john_connected_account_id",
     device_type: "august_lock",
     name: "Back Door",
-    workspace_id: "some_workspace_1",
+    workspace_id: "seed_workspace_1",
   })
 
   db.addConnectedAccount({
     provider: "schlage",
-    workspace_id: "some_workspace_1",
+    workspace_id: "seed_workspace_1",
     user_identifier: {
       email: "jane@example.com",
     },
@@ -73,11 +73,11 @@ export const seed = (db: Database): Seed => {
     connected_account_id: "jane_connected_account_id",
     device_type: "schlage_lock",
     name: "Bathroom Door",
-    workspace_id: "some_workspace_1",
+    workspace_id: "seed_workspace_1",
   })
 
   db.addClientSession({
-    workspace_id: "some_workspace_1",
+    workspace_id: "seed_workspace_1",
     connect_webview_ids: [cw.connect_webview_id],
     connected_account_ids: ["john_connected_account_id"],
     user_identifier_key: "seed_client_session_user",
@@ -86,7 +86,7 @@ export const seed = (db: Database): Seed => {
   return {
     john_connected_account_id: "john_connected_account_id",
     jane_connected_account_id: "jane_connected_account_id",
-    some_workspace_1: "some_workspace_1",
-    some_workspace_2: "some_workspace_2",
+    seed_workspace_1: "seed_workspace_1",
+    seed_workspace_2: "seed_workspace_2",
   }
 }
