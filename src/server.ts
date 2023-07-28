9b8e689fa0014c9669785e8a74783a5b0496ea52
Evan Sosenko <evan@getseam.com>
2023-07-28 16:02:41 -0700

    fix: Handle signals in docker

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,35 @@
 #!/usr/bin/env node
 
-import { argv, env, exit } from "node:process"
+import process, { argv, env, exit } from "node:process"
 
-import { createFake } from "./index.ts"
+import { createFake } from "lib/fake.ts"
+import logger from "lib/logger.ts"
+
+process.on("unhandledRejection", (err) => {
+  logger.error("Unhandled rejection", err)
+  exit(1)
+})
+
+process.on("uncaughtException", (err) => {
+  logger.error("Uncaught exception", err)
+  exit(1)
+})
 
 const main = async (): Promise<void> => {
   const port = env["PORT"] == null ? undefined : Number(env["PORT"])
   const shouldSeed = argv.includes("--seed")
 
   const fake = await createFake()
+
   if (shouldSeed) await fake.seed()
-  await fake.startServer({ port })
-  // eslint-disable-next-line no-console
-  console.log(fake.serverUrl)
+
+  await fake.startServer({ port, signals: ["SIGINT", "SIGUSR2"] })
+
+  logger.info(fake.serverUrl ?? "")
 }
 
 main().catch((err) => {
   // eslint-disable-next-line no-console
-  console.error(err)
+  logger.error(err)
   exit(1)
 })
