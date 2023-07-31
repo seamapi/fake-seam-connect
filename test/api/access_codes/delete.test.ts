5ab61e2a0ae20015d9f1fd90eb3526dd5df7f16c
Mike Wu <hello@mikewu.dev>
2023-07-31 21:55:14 +0900

    feat(AccessCode): add /access_codes/delete

---
+++ test/api/access_codes/delete.test.ts
@@ -0,0 +1,46 @@
+import test, { type ExecutionContext } from "ava"
+
+import { getTestServer } from "fixtures/get-test-server.ts"
+
+test("POST /access_codes/update", async (t: ExecutionContext) => {
+  const { axios, seed, db } = await getTestServer(t)
+  const device_id = seed.ws2.device1_id
+
+  const {
+    data: { access_code },
+  } = await axios.post(
+    "/access_codes/create",
+    {
+      device_id,
+      name: "Test Access Code",
+      code: "1234",
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${seed.ws2.cst}`,
+      },
+    }
+  )
+
+  t.is(access_code.code, "1234")
+
+  const res = await axios.post(
+    "/access_codes/delete",
+    {
+      access_code_id: access_code.access_code_id,
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${seed.ws2.cst}`,
+      },
+    }
+  )
+
+  t.is(res.status, 200)
+
+  const deleted = db.findAccessCode({
+    access_code_id: access_code.access_code_id,
+  })
+
+  t.falsy(deleted) // removed from db
+})
