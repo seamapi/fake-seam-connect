0bab56c27d37580b04bc861f892dfa911d612e7b
Michael Anderjaska <m.anderjaska@gmail.com>
2023-08-23 13:24:15 -0700

    done,need to test

---
+++ src/pages/api/thermostats/climate_setting_schedules/get.ts
@@ -0,0 +1,33 @@
+import { NotFoundException } from "nextlove"
+import { z } from "zod"
+
+import { climate_setting_schedule } from "lib/zod/index.ts"
+
+import { withRouteSpec } from "lib/middleware/with-route-spec.ts"
+
+export default withRouteSpec({
+  auth: "cst_ak_pk",
+  methods: ["GET", "POST"],
+  commonParams: z.object({
+    climate_setting_schedule_id: z.string(),
+    device_id: z.string().optional(),
+  }),
+  jsonResponse: z.object({
+    climate_setting_schedule,
+  }),
+} as const)(async (req, res) => {
+  const { device_id, climate_setting_schedule_id } = req.commonParams
+
+  const climate_setting_schedule = req.db.findClimateSettingSchedule({
+    climate_setting_schedule_id,
+    device_id,
+  })
+
+  if (climate_setting_schedule == null) {
+    throw new NotFoundException({
+      type: "not_found",
+      message: "Climate Setting Schedule Not Found",
+    })
+  }
+  res.status(200).json({ climate_setting_schedule })
+})
