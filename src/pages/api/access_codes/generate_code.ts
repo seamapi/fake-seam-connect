e294657d878ff5a6cf5dc017d43708ac76977eeb
Evan Sosenko <evan@getseam.com>
2023-08-28 16:21:56 -0700

    feat: Add /access_codes/generate_code (#58)
    * Add /access_codes/generate_code
    
    * ci: Generate code
    
    * Fix test
    
    * Generate valid code lengths
    
    * Use get in test
    
    ---------
    
    Co-authored-by: Seam Bot <devops@getseam.com>

---
+++ src/pages/api/access_codes/generate_code.ts
@@ -0,0 +1,39 @@
+import { randomInt } from "crypto"
+import { NotFoundException } from "nextlove"
+import { z } from "zod"
+
+import { withRouteSpec } from "lib/middleware/with-route-spec.ts"
+
+export default withRouteSpec({
+  auth: "cst_ak_pk",
+  methods: ["GET", "POST"],
+  commonParams: z.object({
+    device_id: z.string(),
+  }),
+  jsonResponse: z.object({
+    generated_code: z.object({
+      device_id: z.string(),
+      code: z.string(),
+    }),
+  }),
+} as const)(async (req, res) => {
+  const { device_id } = req.commonParams
+  const device = req.db.devices.find((device) => device.device_id === device_id)
+
+  if (device == null) {
+    throw new NotFoundException({
+      type: "device_not_found",
+      message: "Device not found",
+      data: {
+        device_id,
+      },
+    })
+  }
+
+  res.status(200).json({
+    generated_code: {
+      device_id,
+      code: randomInt(1000, 999999).toString(),
+    },
+  })
+})
---
+++ test/api/access_codes/generate_code.test.ts
@@ -0,0 +1,22 @@
+import test, { type ExecutionContext } from "ava"
+
+import { getTestServer } from "fixtures/get-test-server.ts"
+
+test("GET /access_codes/generate_code", async (t: ExecutionContext) => {
+  const { axios, seed } = await getTestServer(t)
+  const device_id = seed.ws2.device1_id
+  const {
+    data: { generated_code },
+  } = await axios.get("/access_codes/generate_code", {
+    params: {
+      device_id,
+    },
+    headers: {
+      Authorization: `Bearer ${seed.ws2.cst}`,
+    },
+  })
+
+  t.is(generated_code.device_id, seed.ws2.device1_id)
+  t.true(generated_code.code.length >= 4)
+  t.true(generated_code.code.length <= 6)
+})
