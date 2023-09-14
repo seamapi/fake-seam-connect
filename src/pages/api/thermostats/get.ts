c9480920f6b529009b1e909af1006d52cd5d2e80
Tristan B <tristan@infi.im>
2023-09-14 14:35:10 -0600

    Add `thermostats/get` and `thermostatDeviceProperties`

---
+++ src/pages/api/thermostats/get.ts
@@ -0,0 +1,41 @@
+import { NotFoundException } from "nextlove"
+import { z } from "zod"
+
+import { device } from "lib/zod/index.ts"
+
+import { withRouteSpec } from "lib/middleware/with-route-spec.ts"
+
+export default withRouteSpec({
+  auth: "cst_ak_pk",
+  methods: ["GET", "POST"],
+  commonParams: z.object({
+    device_id: z.string().uuid().optional(),
+    name: z.string().optional(),
+  }),
+  jsonResponse: z.object({
+    device,
+  }),
+} as const)(async (req, res) => {
+  const { device_id, name } = req.commonParams
+
+  const device = req.db.devices.find((device) => {
+    if (device_id != null) {
+      return device.device_id === device_id
+    }
+
+    if (name != null) {
+      return device.properties.name === name
+    }
+
+    return false
+  })
+
+  if (device == null) {
+    throw new NotFoundException({
+      type: "device_not_found",
+      message: `Could not find a thermostat with device_id`,
+      data: { device_id, name },
+    })
+  }
+  res.status(200).json({ device })
+})
