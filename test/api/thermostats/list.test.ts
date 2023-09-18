a3a6f2e61a3d4e17fb1a6588db5b27d7b7b6c2b8
Tristan B <tristan@infi.im>
2023-09-18 13:55:17 -0600

    Add `/thermostats/list`

---
+++ test/api/thermostats/list.test.ts
@@ -0,0 +1,22 @@
+import test, { type ExecutionContext } from "ava"
+
+import { getTestServer } from "fixtures/get-test-server.ts"
+import { seed } from "lib/database/seed.ts"
+import { THERMOSTAT_DEVICE_TYPES } from "lib/zod/device.ts"
+
+test("POST /thermostats/list with api key", async (t: ExecutionContext) => {
+  const { axios, db } = await getTestServer(t, { seed: false })
+  const seed_result = seed(db)
+
+  axios.defaults.headers.common.Authorization = `Bearer ${seed_result.seam_apikey1_token}`
+
+  const {
+    data: { thermostats },
+  } = await axios.get("/thermostats/list")
+
+  t.is(thermostats.length, 1)
+
+  thermostats.forEach((thermostat) => {
+    t.true(THERMOSTAT_DEVICE_TYPES.includes(thermostat.device_type))
+  })
+})
