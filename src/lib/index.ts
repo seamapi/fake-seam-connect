11b0c7320ec7da48f20098ff8110a6daa83ea3ed
Evan Sosenko <evan@getseam.com>
2023-10-03 16:08:33 -0700

    feat: Export Seed type

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,4 +1,4 @@
-export type { Database } from "./database/index.ts"
+export type { Database, Seed } from "./database/index.ts"
 export { createDatabase } from "./database/index.ts"
 export { createFake, Fake } from "./fake.ts"
 export { startServer } from "./server.ts"
