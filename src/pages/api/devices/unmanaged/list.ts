0e7319d362f1acb9c7254a9ba0f9dd34b06f7974
Andrii Balitskyi <10balian10@gmail.com>
2023-10-13 18:16:48 +0200

    Add /devices/unmanaged/list + test

---
+++ src/pages/api/devices/unmanaged/list.ts
@@ -0,0 +1,49 @@
+import { z } from "zod"
+
+import { unmanaged_device } from "lib/zod/index.ts"
+
+import { withRouteSpec } from "lib/middleware/with-route-spec.ts"
+import { getUnmanagedDevicesWithFilter } from "lib/util/devices.ts"
+
+import { common_params } from "../list.ts"
+
+export default withRouteSpec({
+  auth: "cst_ak_pk",
+  methods: ["GET", "POST"],
+  commonParams: common_params,
+  jsonResponse: z.object({
+    devices: z.array(unmanaged_device),
+  }),
+} as const)(async (req, res) => {
+  const {
+    device_ids,
+    connected_account_id,
+    connected_account_ids,
+    device_type,
+    device_types,
+    manufacturer,
+  } = req.commonParams
+  const { workspace_id } = req.auth
+
+  let devices = getUnmanagedDevicesWithFilter(req.db, {
+    workspace_id,
+    device_ids,
+    connected_account_id,
+    connected_account_ids,
+    device_type,
+    device_types,
+    manufacturer,
+  })
+
+  if (req.auth.auth_mode === "client_session_token") {
+    const auth_connected_account_ids = req.auth.connected_account_ids
+
+    devices = devices.filter((d) =>
+      auth_connected_account_ids.includes(d.connected_account_id)
+    )
+  }
+
+  res.status(200).json({
+    devices,
+  })
+})
---
+++ test/api/devices/unmanaged/list.test.ts
@@ -0,0 +1,39 @@
+import test, { type ExecutionContext } from "ava"
+
+import { getTestServer } from "fixtures/get-test-server.ts"
+
+test("GET /devices/unmanaged/list", async (t: ExecutionContext) => {
+  const { axios, seed } = await getTestServer(t)
+  const device_id = seed.ws2.device1_id
+
+  let unmanaged_devices_list_res = await axios.get("/devices/unmanaged/list", {
+    headers: {
+      Authorization: `Bearer ${seed.ws2.cst}`,
+    },
+  })
+  t.is(unmanaged_devices_list_res.status, 200)
+  t.true(unmanaged_devices_list_res.data.devices.length === 0)
+
+  await axios.patch(
+    "/devices/update",
+    {
+      device_id,
+      is_managed: false,
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${seed.ws2.cst}`,
+      },
+    }
+  )
+
+  unmanaged_devices_list_res = await axios.get("/devices/unmanaged/list", {
+    headers: {
+      Authorization: `Bearer ${seed.ws2.cst}`,
+    },
+  })
+
+  t.is(unmanaged_devices_list_res.status, 200)
+  t.true(unmanaged_devices_list_res.data.devices.length === 1)
+  t.is(unmanaged_devices_list_res.data.devices[0]?.device_id, device_id)
+})
