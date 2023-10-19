1a1729aafe6df3345f222e55543f96ea42aa3c4d
Andrii Balitskyi <10balian10@gmail.com>
2023-10-19 15:27:33 +0200

    Add /lock/get endpoint

--- src/pages/api/locks/get.ts
+++ src/pages/api/locks/get.ts
@@ -1,39 +1,36 @@
 import { HttpException, NotFoundException } from "nextlove"
 import { z } from "zod"
 
-import { device } from "lib/zod/index.ts"
+import { LOCK_DEVICE_TYPES, device } from "lib/zod/index.ts"
+import type { LockDeviceType } from "lib/zod/index.ts"
 
 import { withRouteSpec } from "lib/middleware/with-route-spec.ts"
-
-export const common_params = z
-  .object({
-    device_id: z.string().optional(),
-    name: z.string().optional(),
-  })
-  .refine(
-    (args) => Boolean(args.device_id) || Boolean(args.name),
-    "Either 'device_id' or 'name' is required"
-  )
+import { common_params } from "../devices/get.ts"
 
 export default withRouteSpec({
   auth: "cst_ak_pk",
   methods: ["GET", "POST"],
   commonParams: common_params,
   jsonResponse: z.object({
-    device,
+    lock: device,
+    device: device,
   }),
 } as const)(async (req, res) => {
   const { device_id, name } = req.commonParams
 
   const device = req.db.devices.find(
-    (d) => d.device_id === device_id || d.properties.name === name
+    (d) =>
+      (d.device_id === device_id || d.properties.name === name) &&
+      LOCK_DEVICE_TYPES.includes(d.device_type as LockDeviceType)
   )
+
   if (device == null) {
     throw new NotFoundException({
       type: "device_not_found",
       message: "Device not found",
     })
   }
+
   if (req.auth.auth_mode === "client_session_token") {
     if (!req.auth.connected_account_ids.includes(device.connected_account_id)) {
       throw new HttpException(401, {
@@ -42,5 +39,6 @@ export default withRouteSpec({
       })
     }
   }
-  res.status(200).json({ device })
+
+  res.status(200).json({ lock: device, device })
 })
