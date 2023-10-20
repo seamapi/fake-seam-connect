5e85df4274b10ba46bccb10e52ae946c3fd7a28f
Andrii Balitskyi <10balian10@gmail.com>
2023-10-20 14:56:02 +0200

    Test the endpoints

---
+++ test/api/workspaces/list.test.ts
@@ -0,0 +1,18 @@
+import test, { type ExecutionContext } from "ava"
+
+import { getTestServer } from "fixtures/get-test-server.ts"
+
+test("GET /workspaces/list", async (t: ExecutionContext) => {
+  const { axios, seed } = await getTestServer(t)
+
+  const {
+    data: { workspaces },
+  } = await axios.get("/workspaces/list", {
+    headers: {
+      Authorization: `Bearer ${seed.ws2.cst}`,
+    },
+  })
+
+  t.is(workspaces.length, 1)
+  t.is(workspaces[0]?.workspace_id, seed.ws2.workspace_id)
+})
