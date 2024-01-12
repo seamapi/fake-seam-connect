0fef7d9f3f6cd9c1e4d1e5878278d9429952266e
Mike <littman@getseam.com>
2024-01-12 11:23:18 -0600

    feat: prepare_endpoint|list_endpoints (#146)

--- src/pages/api/internal/phone/user_identities/prepare_endpoint.ts
+++ src/pages/api/internal/phone/user_identities/prepare_endpoint.ts
@@ -1,3 +1,4 @@
+import { NotFoundException } from "nextlove"
 import { z } from "zod"
 
 import { withRouteSpec } from "lib/middleware/with-route-spec.ts"
@@ -15,6 +16,32 @@ export default withRouteSpec({
   jsonResponse: z.object({
     endpoint: endpoint_schema.optional(),
   }),
-} as const)(async (_req, res) => {
-  res.status(500).end("Not implemented!")
+} as const)(async (req, res) => {
+  const { client_session_id, workspace_id } = req.auth
+  const { custom_sdk_installation_id, endpoint_id } = req.body
+
+  const installation = req.db.getState().getPhoneSdkInstallation({
+    workspace_id,
+    ext_sdk_installation_id: custom_sdk_installation_id,
+    client_session_id,
+  })
+
+  if (installation === undefined) {
+    throw new NotFoundException({
+      message: "Phone SDK installation not found",
+      type: "phone_sdk_installation_not_found",
+    })
+  }
+
+  const endpoint = req.db
+    .getState()
+    .getEndpoints({
+      phone_sdk_installation_id: installation.phone_sdk_installation_id,
+      client_session_id,
+    })
+    .find((endpoint) => endpoint.endpoint_id === endpoint_id)
+
+  res.status(200).json({
+    endpoint,
+  })
 })
---
+++ test/api/internal/phone/user_identities/list_endpoints.test.ts
@@ -0,0 +1,69 @@
+import test from "ava"
+
+import { getTestServer } from "fixtures/get-test-server.ts"
+import { seed } from "lib/database/seed.ts"
+
+test("POST /internal/phone/user_identities/list_endpoints", async (t) => {
+  const { axios, db } = await getTestServer(t, { seed: false })
+  const { seam_cst1_token } = seed(db)
+
+  const client_session = db.getClientSession(seam_cst1_token)
+
+  if (client_session === undefined) {
+    t.fail("Client session not found")
+    return
+  }
+
+  axios.defaults.headers.common.Authorization = `Bearer ${seam_cst1_token}`
+
+  const ext_sdk_installation_id = "ext_sdk_installation_id"
+
+  await axios.post("/internal/phone/user_identities/load_invitations", {
+    custom_sdk_installation_id: ext_sdk_installation_id,
+    phone_os: "android",
+  })
+
+  const {
+    data: { invitations },
+  } = await axios.post("/internal/phone/user_identities/load_invitations", {
+    custom_sdk_installation_id: ext_sdk_installation_id,
+    phone_os: "android",
+  })
+
+  const invitation_code = invitations[0]?.invitation_code ?? ""
+  if (invitation_code === "") {
+    t.fail("Failed to create invite")
+  }
+
+  const {
+    data: { endpoint },
+  } = await axios.post(
+    `/internal/sandbox/${client_session.workspace_id}/assa_abloy/_fake/redeem_invite_code`,
+    {
+      invite_code: invitation_code,
+      endpoint_details: {
+        application_version: "",
+        ble_capability: true,
+        device_manufacturer: "",
+        device_model: "",
+        hce_capability: true,
+        nfc_capability: true,
+        os_version: "",
+        seos_applet_version: "",
+      },
+    },
+  )
+
+  const {
+    data: { endpoints },
+    status,
+  } = await axios.post("/internal/phone/user_identities/list_endpoints", {
+    custom_sdk_installation_id: ext_sdk_installation_id,
+  })
+
+  t.is(status, 200)
+  t.like(endpoints[0], {
+    endpoint_id: endpoint.endpoint_id,
+    is_active: endpoint.status === "ACTIVE",
+  })
+})
