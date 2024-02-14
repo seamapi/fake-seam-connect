79beaf5a5e1e4494b294bb9eff9380e9bd349d50
Ethan Lee <ethanplee14@gmail.com>
2024-02-14 12:24:26 -0800

    fix: remove invitation id from assa abloy endpoint

--- src/lib/zod/endpoints.ts
+++ src/lib/zod/endpoints.ts
@@ -8,7 +8,6 @@ export const endpoint_schema_hid = z.object({
 export const endpoint_schema_assa_abloy = z.object({
   endpoint_type: z.literal("assa_abloy_credential_service"),
   endpoint_id: z.string(),
-  invitation_id: z.string(),
   is_active: z.boolean(),
   seos_tsm_endpoint_id: z.number().nullable(),
   assa_abloy_credential_service_id: z.string(),
