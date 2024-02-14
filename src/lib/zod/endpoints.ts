4c648f700436f48e7608ed1a9ffa23aa73b774e0
Ethan Lee <ethanplee14@gmail.com>
2024-02-14 13:53:58 -0800

    feat: fix test and types

--- src/lib/zod/endpoints.ts
+++ src/lib/zod/endpoints.ts
@@ -8,14 +8,24 @@ export const endpoint_schema_hid = z.object({
 export const endpoint_schema_assa_abloy = z.object({
   endpoint_type: z.literal("assa_abloy_credential_service"),
   endpoint_id: z.string(),
+  invitation_id: z.string(),
   is_active: z.boolean(),
   seos_tsm_endpoint_id: z.number().nullable(),
   assa_abloy_credential_service_id: z.string(),
 })
+
+export const public_endpoint_schema_assa_abloy =
+  endpoint_schema_assa_abloy.omit({ invitation_id: true })
 
 export const endpoint_schema = z.discriminatedUnion("endpoint_type", [
   endpoint_schema_hid,
   endpoint_schema_assa_abloy,
 ])
 
+export const public_endpoint_schema = z.discriminatedUnion("endpoint_type", [
+  endpoint_schema_hid,
+  public_endpoint_schema_assa_abloy,
+])
+
 export type Endpoint = z.infer<typeof endpoint_schema>
+export type PublicEndpoint = z.infer<typeof public_endpoint_schema>
