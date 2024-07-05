39aeb99b23b851827fcd15af60e8cabcb9953e69
Andrii Balitskyi <84702959+andrii-balitskyi@users.noreply.github.com>
2024-07-05 14:17:37 +0200

    feat: Add `/_fake/simulate_workspace_outage` (#201)
    * Add simulate workspace outage endpoint
    
    * Add test
    
    * ci: Format code
    
    * ci: Format code
    
    * Fix nullable condition
    
    ---------
    
    Co-authored-by: Seam Bot <devops@getseam.com>

---
+++ src/pages/api/_fake/simulate_workspace_outage.ts
@@ -0,0 +1,34 @@
+import type { Routes } from "@seamapi/fake-seam-connect"
+import { NotFoundException } from "nextlove"
+import { z } from "zod"
+
+import { withRouteSpec } from "lib/middleware/with-route-spec.ts"
+
+export default withRouteSpec({
+  auth: "none",
+  methods: ["POST"],
+  jsonBody: z.object({
+    workspace_id: z.string(),
+    routes: z.array(z.string()),
+  }),
+  jsonResponse: z.object({}),
+} as const)(async (req, res) => {
+  const { workspace_id, routes } = req.body
+
+  const workspace = req.db.workspaces.find(
+    (w) => w.workspace_id === workspace_id,
+  )
+
+  if (workspace == null) {
+    throw new NotFoundException({
+      type: "workspace_not_found",
+      message: "Workspace not found",
+    })
+  }
+
+  req.db.simulateWorkspaceOutage(workspace_id, {
+    routes: routes as Array<keyof Routes>,
+  })
+
+  res.status(200).json({})
+})
