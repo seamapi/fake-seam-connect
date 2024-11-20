c321e806d01072202cfcdfa7c466ea3e6358f117
Andrii Balitskyi <10balian10@gmail.com>
2024-11-20 16:47:50 +0100

    Fix src/pages/api/client_sessions/get_or_create.ts

--- src/pages/api/client_sessions/get_or_create.ts
+++ src/pages/api/client_sessions/get_or_create.ts
@@ -1,4 +1,4 @@
-import { BadRequestException } from "nextlove"
+import { BadRequestException, HttpException } from "nextlove"
 import { z } from "zod"
 
 import { withRouteSpec } from "lib/middleware/index.ts"
@@ -6,7 +6,7 @@ import { withRouteSpec } from "lib/middleware/index.ts"
 import { client_session } from "lib/zod/client_session.ts"
 
 export default withRouteSpec({
-  auth: ["client_session", "pat_with_workspace", "console_session", "api_key"],
+  auth: "none",
   methods: ["POST", "PUT"],
   middlewares: [],
   jsonBody: z.union([
@@ -24,9 +24,13 @@ export default withRouteSpec({
     ok: z.literal(true),
   }),
 } as const)(async (req, res) => {
-  // TODO if pubkey, fail if connect_webview_ids or connected_account_ids are provided
-
-  const { workspace_id } = req.auth
+  const publishable_key = req.headers["seam-publishable-key"] as string | null
+  if (publishable_key == null) {
+    throw new BadRequestException({
+      type: "publishable_key_header_required",
+      message: "Seam-Publishable-Key header required",
+    })
+  }
 
   const user_identifier_key =
     req.body?.user_identifier_key ??
@@ -35,39 +39,62 @@ export default withRouteSpec({
 
   if (user_identifier_key == null) {
     throw new BadRequestException({
-      type: "missing_user_identifier_key",
-      message: "You must provide a user_identifier_key",
+      type: "publishable_key_requires_user_identifier_key",
+      message: "Must provide a user_identifier_key with a seam-publishable-key",
     })
   }
 
-  if (user_identifier_key != null) {
-    const existing_cs = req.db.client_sessions.find(
-      (cst) =>
-        cst.user_identifier_key === user_identifier_key &&
-        cst.workspace_id === workspace_id,
-    )
-    if (existing_cs != null) {
-      const device_count = req.db.devices.filter(
-        (d) =>
-          d?.connected_account_id !== undefined &&
-          existing_cs.connected_account_ids.includes(d.connected_account_id),
-      ).length
+  const { connect_webview_ids, connected_account_ids } = req.body
 
-      res.json({
-        client_session: {
-          ...existing_cs,
-          device_count,
-        },
-        ok: true,
-      })
-      return
-    }
+  if (
+    publishable_key != null &&
+    // eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing
+    (connect_webview_ids != null || connected_account_ids != null)
+  ) {
+    throw new HttpException(403, {
+      type: "publishable_keys_must_create_empty_client_sessions",
+      message:
+        "You cannot specify connect_webview_ids or connected_account_ids when using a publishable key",
+    })
+  }
+
+  const workspace = req.db.workspaces.find(
+    (ws) => ws.publishable_key === publishable_key,
+  )
+  if (workspace == null)
+    throw new BadRequestException({
+      type: "workspace_not_found",
+      message: "Cannot find workspace associated with this publishable_key",
+    })
+
+  const { workspace_id } = workspace
+
+  const existing_cs = req.db.client_sessions.find(
+    (cst) =>
+      cst.user_identifier_key === user_identifier_key &&
+      cst.workspace_id === workspace_id,
+  )
+  if (existing_cs != null) {
+    const device_count = req.db.devices.filter(
+      (d) =>
+        d?.connected_account_id !== undefined &&
+        existing_cs.connected_account_ids.includes(d.connected_account_id),
+    ).length
+
+    res.json({
+      client_session: {
+        ...existing_cs,
+        device_count,
+      },
+      ok: true,
+    })
+    return
   }
 
   const client_session = req.db.addClientSession({
     workspace_id,
-    connect_webview_ids: req.body?.connect_webview_ids,
-    connected_account_ids: req.body?.connected_account_ids,
+    connect_webview_ids,
+    connected_account_ids,
     user_identifier_key,
   })
 
