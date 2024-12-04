ebae3a33dfcecb25fbbe9b2585361a74338a3ba7
Andrii Balitskyi <10balian10@gmail.com>
2024-12-04 17:30:45 +0100

    Improve withAccessToken auth middleware

--- src/lib/middleware/with-access-token.ts
+++ src/lib/middleware/with-access-token.ts
@@ -1,4 +1,9 @@
-import { HttpException, type Middleware, UnauthorizedException } from "nextlove"
+import {
+  BadRequestException,
+  HttpException,
+  type Middleware,
+  UnauthorizedException,
+} from "nextlove"
 import type { AuthenticatedRequest } from "src/types/authenticated-request.ts"
 
 import type { Database } from "lib/database/index.ts"
@@ -36,13 +41,15 @@ export const withAccessToken =
   (next) =>
   async (req, res) => {
     const token = req.headers.authorization?.split("Bearer ")?.[1]
-    if (token == null) return res.status(401).end("Unauthorized")
+    if (!token) {
+      throw new UnauthorizedException({
+        type: "unauthorized",
+        message:
+          "No token found in header (did you mean to add Authorization?)",
+      })
+    }
 
-    const is_at = token.includes("seam_at")
-    const long_token = token.split("_")?.[2]
-    const short_token = token.split("_")?.[1]
-
-    if (!is_at) {
+    if (!token.startsWith("seam_at")) {
       throw new UnauthorizedException({
         type: "unauthorized",
         message: "Access tokens must start with seam_at",
@@ -55,62 +62,62 @@ export const withAccessToken =
       !Array.isArray(workspace_id_from_header)
         ? workspace_id_from_header
         : ""
+    const is_workspace_id_provided =
+      workspace_id != null && workspace_id.length > 0
 
-    if (
-      (workspace_id == null || workspace_id.length === 0) &&
-      is_workspace_id_required
-    ) {
-      throw new UnauthorizedException({
+    if (!is_workspace_id_provided && is_workspace_id_required) {
+      throw new BadRequestException({
         type: "missing_workspace_id",
-        message: "Workspace ID is required",
+        message:
+          "When using access token authentication, you must provide the Seam-Workspace header",
       })
     }
 
-    if (short_token == null || long_token == null)
-      return res.status(400).end("malformed token")
+    const long_token_hash = extractLongTokenHash(token)
 
-    if (is_at) {
-      const long_token_hash = extractLongTokenHash(token)
-
-      const access_token = req.db.access_tokens.find(
-        (token) => token.long_token_hash === long_token_hash,
-      )
-      if (access_token == null)
-        throw new UnauthorizedException({
-          type: "access_token_not_found",
-          message: "Access token not found",
-        })
-
-      if (is_workspace_id_required) {
-        ;(req.auth as unknown as Extract<
-          AuthenticatedRequest["auth"],
-          { type: "access_token" }
-        >) = {
-          type: "access_token",
-          workspace_id,
-        }
-      } else {
-        ;(req.auth as unknown as Extract<
-          AuthenticatedRequest["auth"],
-          { type: "access_token_without_workspace" }
-        >) = {
-          type: "access_token_without_workspace",
-          user_id: access_token.user_id,
-        }
-      }
-
-      // Cannot run middleware after auth middleware.
-      // UPSTREAM: https://github.com/seamapi/nextlove/issues/118
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-      return withSimulatedOutage(next as unknown as any)(
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-        req as unknown as any,
-        res,
-      )
+    const access_token = req.db.access_tokens.find(
+      (token) =>
+        token.long_token_hash === long_token_hash &&
+        (!is_workspace_id_provided || token.workspace_id === workspace_id),
+    )
+    if (access_token == null) {
+      throw new UnauthorizedException({
+        type: "unauthorized",
+        message: "Access token not found",
+      })
     }
 
-    throw new HttpException(500, {
-      type: "unknown_auth_mode",
-      message: "Unknown Auth Mode",
-    })
+    if (is_workspace_id_required) {
+      ;(req.auth as unknown as Extract<
+        AuthenticatedRequest["auth"],
+        { type: "access_token" }
+      >) = {
+        type: "access_token",
+        access_token_id: access_token.access_token_id,
+        access_token_short_token: access_token.short_token,
+        token,
+        user_id: access_token.user_id,
+        workspace_id: access_token.workspace_id,
+      }
+    } else {
+      ;(req.auth as unknown as Extract<
+        AuthenticatedRequest["auth"],
+        { type: "access_token_without_workspace" }
+      >) = {
+        type: "access_token_without_workspace",
+        access_token_id: access_token.access_token_id,
+        access_token_short_token: access_token.short_token,
+        token,
+        user_id: access_token.user_id,
+      }
+    }
+
+    // Cannot run middleware after auth middleware.
+    // UPSTREAM: https://github.com/seamapi/nextlove/issues/118
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+    return withSimulatedOutage(next as unknown as any)(
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+      req as unknown as any,
+      res,
+    )
   }
