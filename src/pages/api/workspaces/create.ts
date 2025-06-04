28ce19da6f0cf4dd0bccbfea53b4ea3f9a1937e7
Andrii Balitskyi <10balian10@gmail.com>
2025-06-04 14:57:18 +0200

    Remove /internal endpoint

--- src/pages/api/workspaces/create.ts
+++ src/pages/api/workspaces/create.ts
@@ -1,8 +1,15 @@
 import { z } from "zod"
 
 import { withRouteSpec } from "lib/middleware/with-route-spec.ts"
-import { workspace } from "lib/zod/workspace.ts"
-import { jsonBody } from "pages/api/internal/workspaces/create.ts"
+import { optional_hex_color_code, workspace } from "lib/zod/workspace.ts"
+
+export const jsonBody = z.object({
+  workspace_name: z.string(),
+  connect_partner_name: z.string(),
+  is_sandbox: z.boolean(),
+  webview_primary_button_color: optional_hex_color_code,
+  webview_logo_shape: z.enum(["circle", "square"]).optional(),
+})
 
 export const route_spec = {
   methods: ["POST"],
