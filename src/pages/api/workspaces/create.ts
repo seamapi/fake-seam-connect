c277ccea2e40cb1f1d50ac8d8a5106f29e9e8aa7
Andrii Balitskyi <10balian10@gmail.com>
2025-06-04 14:58:33 +0200

    Update hex_color_code schema

--- src/pages/api/workspaces/create.ts
+++ src/pages/api/workspaces/create.ts
@@ -1,13 +1,13 @@
 import { z } from "zod"
 
 import { withRouteSpec } from "lib/middleware/with-route-spec.ts"
-import { optional_hex_color_code, workspace } from "lib/zod/workspace.ts"
+import { hex_color_code, workspace } from "lib/zod/workspace.ts"
 
 export const jsonBody = z.object({
   workspace_name: z.string(),
   connect_partner_name: z.string(),
   is_sandbox: z.boolean(),
-  webview_primary_button_color: optional_hex_color_code,
+  webview_primary_button_color: hex_color_code.optional(),
   webview_logo_shape: z.enum(["circle", "square"]).optional(),
 })
 
